ebb3cf4cc0816fff2ca3838c7bfe5806768a2e1f
jake <jacobmurleyis@gmail.com>
2020-10-24 11:38:45 -0400

    Added Cheerio to dependencies

--- server/Webscraper/Axios.js
+++ server/Webscraper/Axios.js
@@ -1,10 +1,6 @@
-// pl-scraper.js
-
 const axios = require('axios');
-const cheerio = require('cheerio');
-
+const cheerio = require('cheerio')
 const url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv';
-console.log("I'm here now")
 //<div class="col-xs-12 col-md-12 outer--box outer--box--left">
 axios(url)
   .then(response => {

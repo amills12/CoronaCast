1bad7c89dec6c1956db49a295d665a8dd4290204
Anton <antonlivingston@ufl.edu>
2020-12-05 16:40:38 -0500

    ...

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -29,7 +29,7 @@ useEffect(() => {
   if (user.name === "alexandermills@ufl.edu" || user.name === "coronacast.dev@gmail.com" || user.name === "antonlivingston@ufl.edu") {
     setAdmin(true);
   }}
-}); 
+}, [isAuthenticated, checked, user]); 
 
   if (isLoading) return <Loading/>
 
